95d9494b9546bfb2f60f768544cf5ebab71406df
Dennis Tei-Muno <munodennis@gmail.com>
2024-11-26 00:10:26 -0600

    Create practice.sh

---
+++ practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+az vm create \
+    --resource-group $MY_RESOURCE_GROUP_NAME \
+    --name $MY_VM_NAME \
+    --image $MY_VM_IMAGE \
+    --admin-username $MY_USERNAME \
+    --assign-identity \
+    --generate-ssh-keys \
+    --public-ip-sku Standard
